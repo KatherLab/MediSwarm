2bc06ee3624b2aaba1aff5db07c7d57c84bf112a
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2024-07-02 12:32:33 +0000

    use unittest discovery

--- _runTestsInsideDocker.sh
+++ _runTestsInsideDocker.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-# TODO adapt if tests are moved
 cd controller/controller
 
-# TODO also test client
-# TODO let unittest do test discovery
-
-for testcase in swarm_server_ctl.py swarm_client_ctl.py
-do
-    coverage run $testcase
-    coverage report -m
-    rm .coverage
-done
+python3 -m coverage run -m unittest discover
+coverage report -m
+rm .coverage
