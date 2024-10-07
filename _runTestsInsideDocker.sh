4edbd8fb163699085c51c7f4c19a2e0999cdcbd5
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2024-10-07 15:04:29 +0200

    run simulation mode with not-yet-minimal example together with the unit tests

--- _runTestsInsideDocker.sh
+++ _runTestsInsideDocker.sh
@@ -5,10 +5,13 @@
 # ./runtest.sh
 # cd ..
 
+# run unit tests of ODELIA swarm learning and report coverage
 export MPLCONFIGDIR=/tmp
-
 cd controller/controller
-
 python3 -m coverage run --source=. -m unittest discover
 coverage report -m
 rm .coverage
+
+# run simulation mode for (not yet) minimal example
+cd /workspace
+nvflare simulator -w /tmp/minimal_training -n 2 -t 2 application/test_jobs/minimal_training -c simulated_node_0,simulated_node_1
