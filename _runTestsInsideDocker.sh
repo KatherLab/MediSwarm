ad82b777539665080c5646c7b34254503970ed96
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2024-10-11 17:18:49 +0200

    report coverage for nvflare tests (commented)

--- _runTestsInsideDocker.sh
+++ _runTestsInsideDocker.sh
@@ -2,13 +2,12 @@
 
 # run NVFlare's unit tests (takes about 2 minutes), does not include NVFlare's integration tests yet
 # cd nvflare
-# ./runtest.sh
+# ./runtest.sh -c -r
+# coverage report -m
 # cd ..
 
 export MPLCONFIGDIR=/tmp
-
 cd tests/unit_tests/controller
-
 python3 -m coverage run --source=/workspace/controller/controller -m unittest discover
 coverage report -m
 rm .coverage
