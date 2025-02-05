4b9e9a0fd4689a40268661317befb03a5206bbf0
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2025-02-05 08:36:57 +0100

    run swarm or test/local training controlled by flag in minimal example

--- application/jobs/minimal_training_pytorch_cnn/app/custom/main.py
+++ application/jobs/minimal_training_pytorch_cnn/app/custom/main.py
@@ -1,12 +1,20 @@
+#!/usr/bin/env python3
+
 import nvflare.client.lightning as flare
 import nvflare.client as flare_util
 import torch
 
 import minimal_training
 
-flare_util.init()
+FLARE_MODE = "swarm"
 
-SITE_NAME=flare.get_site_name()
+if FLARE_MODE == "swarm":
+    flare_util.init()
+    SITE_NAME=flare.get_site_name()
+elif FLARE_MODE == "preflight_check" or FLARE_MODE == "local_training":
+    SITE_NAME="site_name_unset"
+else:
+    raise Exception(f"Illegal FLARE_MODE {FLARE_MODE}")
 
 
 def main():
@@ -17,15 +25,19 @@ def main():
     try:
         data_module, model, checkpointing, trainer = minimal_training.prepare_training(logger)
 
-        flare.patch(trainer)  # Patch trainer to enable swarm learning
-        torch.autograd.set_detect_anomaly(True)
+        if FLARE_MODE == "swarm":
+            flare.patch(trainer)  # Patch trainer to enable swarm learning
+            torch.autograd.set_detect_anomaly(True)
 
-        logger.info(f"Site name: {SITE_NAME}")
+            logger.info(f"Site name: {SITE_NAME}")
 
-        while flare.is_running():
-            input_model = flare.receive()
-            logger.info(f"Current round: {input_model.current_round}")
+            while flare.is_running():
+                input_model = flare.receive()
+                logger.info(f"Current round: {input_model.current_round}")
 
+                minimal_training.validate_and_train(logger, data_module, model, trainer)
+
+        elif FLARE_MODE == "preflight_check" or FLARE_MODE == "local_training":
             minimal_training.validate_and_train(logger, data_module, model, trainer)
 
         minimal_training.finalize_training(logger, model, checkpointing, trainer)
