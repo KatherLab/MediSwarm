9b1ad8ace504aea3762ed64c5816da26196a99e5
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2025-02-27 15:09:48 +0100

    Copy code to nfcore image and deploy swarm training jobs (via the admin console) from the container rather than from a mounted location on the host

--- buildDockerImages.sh
+++ buildDockerImages.sh
@@ -6,14 +6,10 @@ if ! git diff --quiet || ! git diff --staged --quiet ; then
    exit 1
 fi
 
-export DOCKER_BUILT_FROM_MEDISWARM_REVISION=`git rev-parse HEAD`
-
-sed -i 's/DOCKER_BUILT_FROM_MEDISWARM_REVISION  # this should be replaced by buildDockerImages.sh/'$DOCKER_BUILT_FROM_MEDISWARM_REVISION'/' docker_config/master_template.yml
 docker build -t nvflare-pt-dev:nfcore . -f docker_config/Dockerfile_nfcore
 # TODO enable tagging and pushing
 # docker tag nvflare-pt-dev:nfcore jefftud/nvflare-pt-dev:nfcore
 # docker push jefftud/nvflare-pt-dev:nfcore
-sed -i 's/'$DOCKER_BUILT_FROM_MEDISWARM_REVISION'/DOCKER_BUILT_FROM_MEDISWARM_REVISION  # this should be replaced by buildDockerImages.sh/' docker_config/master_template.yml
 
 docker build -t nvflare-pt-dev:3dcnn . -f docker_config/Dockerfile_3dcnn
 # docker tag nvflare-pt-dev:3dcnn jefftud/nvflare-pt-dev:3dcnn
