9a9ae7e15db82097c297a06438860e1ad6dd8623
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2025-02-28 14:33:46 +0100

    joint Docker image intended for server and client

--- buildStartupKits.sh
+++ buildStartupKits.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-docker build -t nvflare-pt-dev:nfcore . -f docker_config/Dockerfile_nfcore
-docker run --rm -it -u $(id -u):$(id -g) -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group -v ./:/workspace/ -w /workspace/ nvflare-pt-dev:nfcore /bin/bash -c "nvflare provision -p $1"
+DOCKER_IMAGE=nvflare-pt-dev:odelia
+
+# docker build -t $DOCKER_IMAGE . -f docker_config/Dockerfile_nfcore
+docker run --rm -it -u $(id -u):$(id -g) -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group -v ./:/workspace/ -w /workspace/ $DOCKER_IMAGE /bin/bash -c "nvflare provision -p $1"
