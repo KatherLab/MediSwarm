1d8801d04e841c637b476858e2c4c6fb13470a0b
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2025-02-28 14:33:46 +0100

    joint Docker image intended for server and client

--- buildDockerImages.sh
+++ buildDockerImages.sh
@@ -6,11 +6,5 @@ if ! git diff --quiet || ! git diff --staged --quiet ; then
    exit 1
 fi
 
-docker build -t nvflare-pt-dev:nfcore . -f docker_config/Dockerfile_nfcore
+docker build -t nvflare-pt-dev:odelia . -f docker_config/Dockerfile_ODELIA
 # TODO enable tagging and pushing
-# docker tag nvflare-pt-dev:nfcore jefftud/nvflare-pt-dev:nfcore
-# docker push jefftud/nvflare-pt-dev:nfcore
-
-docker build -t nvflare-pt-dev:3dcnn . -f docker_config/Dockerfile_3dcnn
-# docker tag nvflare-pt-dev:3dcnn jefftud/nvflare-pt-dev:3dcnn
-# docker push jefftud/nvflare-pt-dev:3dcnn
