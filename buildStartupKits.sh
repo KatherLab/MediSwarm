0273f833acdebbf666aadc481bb9e1e963ea047d
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2025-03-13 13:58:22 +0100

    use ODELIA image version number also in swarm project name

--- buildStartupKits.sh
+++ buildStartupKits.sh
@@ -10,7 +10,7 @@ DOCKER_IMAGE=jefftud/odelia:$VERSION
 
 # TODO should the version number also be encoded (and replaced automatically) in the swarm project name?
 
-sed -i 's#docker_image: jefftud/odelia:__REPLACED_BY_CURRENT_VERSION_NUMBER_WHEN_BUILDING_STARTUP_KITS__#docker_image: jefftud/odelia:'$VERSION'#' $1
+sed -i 's#__REPLACED_BY_CURRENT_VERSION_NUMBER_WHEN_BUILDING_STARTUP_KITS__#'$VERSION'#' $1
 
 docker run --rm -it -u $(id -u):$(id -g) -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group -v ./:/workspace/ -w /workspace/ $DOCKER_IMAGE /bin/bash -c "nvflare provision -p $1"
 
