24fbf9227e75a237ab58b3a41f5d4724a90d7517
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2025-03-17 14:54:45 +0100

    removed outdated todo

--- buildStartupKits.sh
+++ buildStartupKits.sh
@@ -8,8 +8,6 @@ fi
 VERSION=`tail -n 1 odelia_image.version`
 DOCKER_IMAGE=jefftud/odelia:$VERSION
 
-# TODO should the version number also be encoded (and replaced automatically) in the swarm project name?
-
 sed -i 's#__REPLACED_BY_CURRENT_VERSION_NUMBER_WHEN_BUILDING_STARTUP_KITS__#'$VERSION'#' $1
 
 docker run --rm -it -u $(id -u):$(id -g) -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group -v ./:/workspace/ -w /workspace/ $DOCKER_IMAGE /bin/bash -c "nvflare provision -p $1"
