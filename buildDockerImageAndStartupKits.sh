31e7583b44ae13bf8ff0ff153b4eec60b084de7e
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2025-03-18 16:46:15 +0100

    combined scripts to build Docker image and startup kits into one

--- buildDockerImageAndStartupKits.sh
+++ buildDockerImageAndStartupKits.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$#" -ne 1 ]; then
-    echo "Usage: buildStartupKits.sh SWARM_PROJECT.yml"
+    echo "Usage: buildDockerImageAndStartupKits.sh SWARM_PROJECT.yml"
     exit 1
 fi
 
@@ -14,8 +14,12 @@ fi
 VERSION=`./getVersionNumber.sh`
 DOCKER_IMAGE=jefftud/odelia:$VERSION
 
+docker build -t $DOCKER_IMAGE . -f docker_config/Dockerfile_ODELIA
+
 sed -i 's#__REPLACED_BY_CURRENT_VERSION_NUMBER_WHEN_BUILDING_STARTUP_KITS__#'$VERSION'#' $1
 
 docker run --rm -it -u $(id -u):$(id -g) -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group -v ./:/workspace/ -w /workspace/ $DOCKER_IMAGE /bin/bash -c "nvflare provision -p $1 && ./_generateStartupKitArchives.sh $1 $VERSION"
 
 sed -i 's#'$VERSION'#__REPLACED_BY_CURRENT_VERSION_NUMBER_WHEN_BUILDING_STARTUP_KITS__#' $1
+
+echo "If you wish, manually push $DOCKER_IMAGE now"
