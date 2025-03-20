3de404a734aae39d214a5077bd5adc26fe224af8
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2025-03-20 17:48:22 +0100

    create a clean version of the source code repository clone before and to be used as build context for docker build

--- buildDockerImageAndStartupKits.sh
+++ buildDockerImageAndStartupKits.sh
@@ -14,7 +14,18 @@ fi
 VERSION=`./getVersionNumber.sh`
 DOCKER_IMAGE=jefftud/odelia:$VERSION
 
-docker build -t $DOCKER_IMAGE . -f docker_config/Dockerfile_ODELIA
+# prepare clean version of source code repository clone for building Docker image
+CWD=`pwd`
+CLEAN_SOURCE_DIR=`mktemp -d`
+cp -r . $CLEAN_SOURCE_DIR/
+cd $CLEAN_SOURCE_DIR
+git clean -f .
+chmod a+rX . -R
+cd $CWD
+
+docker build -t $DOCKER_IMAGE $CLEAN_SOURCE_DIR -f docker_config/Dockerfile_ODELIA --build-arg CLEAN_SOURCE_DIR=$CLEAN_SOURCE_DIR
+
+rm -rf $CLEAN_SOURCE_DIR
 
 ./_buildStartupKits.sh $1 $VERSION
 
