0137261471c9a81935f4734fe8caa2393136836d
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2025-05-08 10:41:57 +0200

    use script in image for code verification test that training does not start if code is different

--- _runTestsInsideDocker.sh
+++ _runTestsInsideDocker.sh
@@ -27,11 +27,12 @@ nvflare simulator -w /tmp/minimal_training_pytorch_cnn -n 2 -t 2 application/job
 
 if [[ ! -z $(grep "MediSwarm code verification succeeded" /scratch/minimal_training_pytorch_cnn_sim.log) ]] && \
    [[ ! -z $(grep "Round 4 started" /scratch/minimal_training_pytorch_cnn_sim.log) ]]; then
-    echo "Simulation mode succeeded."
+    echo "✅ Simulation mode succeeded."
 else
-    echo "Simulation mode failed."
+    echo "❌ Simulation mode failed."
     exit 1
 fi
+rm /scratch/minimal_training_pytorch_cnn_sim.log
 
 # run proof-of-concept mode for minimal example
 cd /MediSwarm
@@ -49,8 +50,48 @@ nvflare poc stop
 
 if [[ ! -z $(grep "MediSwarm code verification succeeded" /scratch/minimal_training_pytorch_cnn_poc.log) ]] && \
    [[ ! -z $(grep "Round 4 started" /scratch/minimal_training_pytorch_cnn_poc.log) ]]; then
-    echo "Proof-of-concept mode succeeded."
+    echo "✅ Proof-of-concept mode succeeded."
 else
-    echo "Proof-of-concept mode failed."
+    echo "❌ Proof-of-concept mode failed."
     exit 1
 fi
+rm /scratch/minimal_training_pytorch_cnn_poc.log
+
+# check, via simulation mode, that differences between intended and distributed code are detected
+# note that we cannot directly modify only the code to be distributed compared to the code in the image here
+cd /MediSwarm
+export TRAINING_MODE="swarm"
+cp -r application/jobs/minimal_training_pytorch_cnn application/jobs/minimal_training_pytorch_cnn_modified
+echo "# modification" >> application/jobs/minimal_training_pytorch_cnn_modified/app/custom/main.py
+# but leave the pointer to the original code as is so that we have a discrepancy
+
+nvflare simulator -w /tmp/minimal_training_pytorch_cnn_modified -n 2 -t 2 application/jobs/minimal_training_pytorch_cnn_modified -c simulated_node_0,simulated_node_1 | tee /scratch/minimal_training_pytorch_cnn_modified_sim.log
+
+if [[ ! -z $(grep "MediSwarm code verification FAILED" /scratch/minimal_training_pytorch_cnn_modified_sim.log) ]] && \
+   [[ -z $(grep "Round 1 started" /scratch/minimal_training_pytorch_cnn_modified_sim.log) ]]; then
+    echo "✅ Code modification detected successfully. (Execution exceptions in the output above are expected)"
+else
+    echo "❌ Code modification not detected."
+    exit 1
+fi
+rm /scratch/minimal_training_pytorch_cnn_modified_sim.log
+
+nvflare poc start -ex admin@nvidia.com | tee /scratch/minimal_training_pytorch_cnn_modified_poc.log &
+sleep 15
+echo "Will submit job now after sleeping 15 seconds to allow the background process to complete"
+nvflare job submit -j application/jobs/minimal_training_pytorch_cnn_modified
+sleep 30
+echo "Will shut down now after sleeping 30 seconds to allow the background process to complete"
+sleep 2
+nvflare poc stop
+
+if [[ ! -z $(grep "MediSwarm code verification FAILED" /scratch/minimal_training_pytorch_cnn_modified_poc.log) ]] && \
+   [[ -z $(grep "Round 1 started" /scratch/minimal_training_pytorch_cnn_modified_poc.log) ]]; then
+    echo "✅ Code modification detected successfully. (Execution exceptions in the output above are expected)"
+else
+    echo "❌ Code modification not detected."
+    exit 1
+fi
+rm /scratch/minimal_training_pytorch_cnn_modified_poc.log
+
+rm -rf application/jobs/minimal_training_pytorch_cnn_modified
--- _verifyCodeIntegrity.sh
+++ _verifyCodeIntegrity.sh
@@ -2,8 +2,15 @@
 
 # This script should be called only at the beginning of a swarm training.
 
+if [ ! -f custom/MediSwarmAPCFolderName.txt ]; then
+  echo "MediSwarm APC folder name not found"
+  exit 1
+fi
+APC_FOLDER_NAME_=`cat custom/MediSwarmAPCFolderName.txt`
+APC_FOLDER_NAME=`basename $APC_FOLDER_NAME_`
+
 DISTRIBUTED_CODE_FOLDER=`pwd`
-EXPECTED_CODE_FOLDER=/MediSwarm/application/jobs/3dcnn_ptl/app
+EXPECTED_CODE_FOLDER=/MediSwarm/application/jobs/$APC_FOLDER_NAME/app
 
 cd $EXPECTED_CODE_FOLDER
 find . \( -name "*.py" -o -name "*.conf" \) -exec shasum {} \; | sort > /tmp/expected_sha_sums.txt
@@ -11,7 +18,7 @@ find . \( -name "*.py" -o -name "*.conf" \) -exec shasum {} \; | sort > /tmp/exp
 cd $DISTRIBUTED_CODE_FOLDER
 COMPARISON_RESULT=$(shasum -c /tmp/expected_sha_sums.txt)
 
-if [ $? = 0 ]; then
+if [ $? -eq 0 ]; then
   echo "MediSwarm code verification succeeded: " $COMPARISON_RESULT
 else
   echo "MediSwarm code verification FAILED: " $COMPARISON_RESULT
