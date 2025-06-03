29bf6d3cfe293cee91bb35ffd69de818bc57603b
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2025-06-03 11:25:05 +0200

    run 3dcnn_ptl training preflight check with synthetic dataset as part of test (does not work yet)

--- runTestsInDocker.sh
+++ runTestsInDocker.sh
@@ -2,6 +2,8 @@
 
 VERSION=`./getVersionNumber.sh`
 DOCKER_IMAGE=jefftud/odelia:$VERSION
+PROJECT_DIR=workspace/odelia_${VERSION}_dummy_project_for_testing
+CWD=`pwd`
 
 run_tests () {
     docker run -it --rm \
@@ -15,15 +17,35 @@ run_tests () {
            $DOCKER_IMAGE
 }
 
-run_dummy_training () {
+prepare_dummy_trainings () {
+    rm $PROJECT_DIR -rf
     ./_buildStartupKits.sh tests/provision/dummy_project_for_testing.yml $VERSION
-
-    PROJECT_DIR=workspace/odelia_${VERSION}_dummy_project_for_testing
-    cd $PROJECT_DIR/prod_00/client_A/startup/
-    ./docker.sh  --data_dir /tmp/ --scratch_dir /tmp/scratch --GPU all --no_pull --dummy_training
-    cd ../../../../../
-    rm -rf $PROJECT_DIR
 }
 
+run_dummy_training () {
+    cd $PROJECT_DIR/prod_00/client_A/startup/
+    ./docker.sh  --data_dir /tmp/ --scratch_dir /tmp/scratch --GPU all --no_pull --dummy_training
+    cd "$CWD"
+}
+
+run_3dcnn_training_with_synthetic_data_preflight_check () {
+    SYNTHETIC_DATA_DIR=`mktemp -d`
+
+    application/jobs/3dcnn_ptl/app/scripts/create_synthetic_dataset/create_synthetic_dataset.py "$SYNTHETIC_DATA_DIR"
+
+    cd $PROJECT_DIR/prod_00/client_A/startup/
+    ./docker.sh  --data_dir "$SYNTHETIC_DATA_DIR" --scratch_dir /tmp/scratch --GPU all --no_pull --preflight_check
+    rm -rf "$SYNTHETIC_DATA_DIR"
+    cd "$CWD"
+}
+
+cleanup_dummy_trainings () {
+    rm $PROJECT_DIR -rf
+}
+
+
 run_tests
+prepare_dummy_trainings
 run_dummy_training
+run_3dcnn_training_with_synthetic_data_preflight_check
+cleanup_dummy_trainings
