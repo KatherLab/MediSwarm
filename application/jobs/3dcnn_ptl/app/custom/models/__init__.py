ad92d19c3f04894e73fa5296642413d84c55fb22
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2025-06-20 13:23:16 +0200

    removed documentation better moved to respective files

--- application/jobs/3dcnn_ptl/app/custom/models/__init__.py
+++ application/jobs/3dcnn_ptl/app/custom/models/__init__.py
@@ -1,9 +1,5 @@
 """
 This package initializes the necessary modules and classes for the project.
-
-Modules:
-    base_model: Contains basic models including VeryBasicModel, BasicModel, and BasicClassifier.
-    resnet: Contains the ResNet model implementation.
 """
 
 from .base_model import VeryBasicModel, BasicModel, BasicClassifier, BasicRegression
