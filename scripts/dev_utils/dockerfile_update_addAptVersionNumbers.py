78ca4069d55a807c764b0166aa7e97e91c59a335
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2025-07-30 13:27:08 +0200

    keep argument -y in same line as RUN apt install adapted scripts for easier handling of 'RUN apt install -y' commands

--- scripts/dev_utils/dockerfile_update_addAptVersionNumbers.py
+++ scripts/dev_utils/dockerfile_update_addAptVersionNumbers.py
@@ -3,6 +3,11 @@
 import re
 import sys
 
+LINE_BREAK_IN_COMMAND = ' \\\n    '
+LINE_BREAK_REPLACEMENT = ' λινε βρεακ ρεπλαζεμεντ '
+APT_INSTALL_COMMAND = 'RUN apt install -y'
+APT_INSTALL_REPLACEMENT = 'ΡΥΝ απτ ινσταλλ -υ'
+
 def load_file(filename: str) -> str:
     with open(filename, 'r') as infile:
         return infile.read()
@@ -14,38 +19,34 @@ def save_file(contents: str, filename: str) -> None:
 
 def parse_apt_versions(installlog: str) -> dict:
     versions = {}
-    pattern = re.compile(r'Get:.*? ([a-z0-9\-\+\.]+)(?:/[^ ]*)? ([0-9a-zA-Z\:\~\.\+\-]+) ')
     for line in installlog.splitlines():
-        match = pattern.search(line)
-        if match:
-            package = match.group(1)
-            version = match.group(2)
-            if package in versions and versions[package] != version:
-                print(f'Conflicting versions of {package} found: {versions[package]} and {version} found, using the latter.')
-            versions[package] = version
+        if re.match('.*Get:[0-9]* http.*', line):
+            blocks = line.split(' ')
+            if len(blocks) > 9:
+                package = blocks[6]
+                version = blocks[8]
+                if package in versions and versions[package] != version:
+                    print(f'Conflicting versions of {package} found: {versions[package]} and {version} found, using the latter.')
+                versions[package] = version
     return versions
 
 
-
 def add_apt_versions(dockerfile: str, versions: dict) -> str:
-    dockerfile = dockerfile.replace('RUN apt install', 'RUN_apt_install')
+    dockerfile = dockerfile.replace(LINE_BREAK_IN_COMMAND, LINE_BREAK_REPLACEMENT)
+    dockerfile = dockerfile.replace(APT_INSTALL_COMMAND, APT_INSTALL_REPLACEMENT)
     outlines = []
     for line in dockerfile.splitlines():
-        if line.startswith('RUN_apt_install'):
+        if line.startswith(APT_INSTALL_REPLACEMENT):
             outline = '' + line
             for package, version in versions.items():
                 outline = outline.replace(f' {package} ', f' {package}={version} ')
                 outline = re.sub(f' {package}$', f' {package}={version}', outline)
-            parts = outline.split()
-            if len(parts) > 3:
-                header = " ".join(parts[:3])
-                pkgs = parts[3:]
-                outline = header + " \\\n    " + " \\\n    ".join(pkgs)
             outlines.append(outline)
         else:
             outlines.append(line)
     dockerfile = '\n'.join(outlines) + '\n'
-    dockerfile = dockerfile.replace('RUN_apt_install', 'RUN apt install')
+    dockerfile = dockerfile.replace(APT_INSTALL_REPLACEMENT, APT_INSTALL_COMMAND)
+    dockerfile = dockerfile.replace(LINE_BREAK_REPLACEMENT, LINE_BREAK_IN_COMMAND)
     return dockerfile
 
 
@@ -58,6 +59,9 @@ def report_non_fixed_versions(dockerfile: str, versions: dict) -> None:
 if __name__ == '__main__':
     dockerfile = load_file(sys.argv[1])
     installlog = load_file(sys.argv[2])
+    if LINE_BREAK_REPLACEMENT in dockerfile or APT_INSTALL_REPLACEMENT in dockerfile:
+        raise Exception('Line break replacement {LINE_BREAK_REPLACEMENT} or apt command replacement {APT_INSTALL_REPLACEMENT} in Dockerfile, cannot process it.')
+
     versions = parse_apt_versions(installlog)
     report_non_fixed_versions(dockerfile, versions)
     dockerfile = add_apt_versions(dockerfile, versions)
