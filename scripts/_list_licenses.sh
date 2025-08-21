a54e70572f391613d963350554f4a18d170ae18b
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2025-08-21 10:57:47 +0200

    refactored listing licenses to avoid code duplication

---
+++ scripts/_list_licenses.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# this script is called inside the ODELIA docker containers to list licenses of all pip and apt packages as well as for pre-trained weights
+
+pip-licenses -s -u --order=license
+distro2sbom -s --format json
+grep "DINOv2 code and model weights are released under" /torch_home/hub/facebookresearch_dinov2_main/README.md
