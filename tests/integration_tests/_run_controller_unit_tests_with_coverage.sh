f10e526666bb0d2d0343f616d843375be340c5e2
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2025-08-27 09:54:16 +0200

    refactored tests to be run in Docker: moved to separate scripts

---
+++ tests/integration_tests/_run_controller_unit_tests_with_coverage.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+run_controller_unit_tests_with_coverage () {
+    # run unit tests of ODELIA swarm learning and report coverage
+    export MPLCONFIGDIR=/tmp
+    cd /MediSwarm/tests/unit_tests/controller
+    PYTHONPATH=/MediSwarm/controller/controller python3 -m coverage run --source=/MediSwarm/controller/controller -m unittest discover
+    coverage report -m
+    rm .coverage
+}
+
+run_controller_unit_tests_with_coverage
