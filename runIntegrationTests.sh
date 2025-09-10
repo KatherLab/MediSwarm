5f1a351630cf94f2f783d6157b451cfcabfda31f
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2025-09-10 14:16:22 +0200

    added check whether job ID is logged by server

--- runIntegrationTests.sh
+++ runIntegrationTests.sh
@@ -204,9 +204,10 @@ run_dummy_training_in_swarm () {
 
     cd "$PROJECT_DIR"/prod_00/localhost/startup
     CONSOLE_OUTPUT=nohup.out
-    for EXPECTED_OUTPUT in 'Total clients: 2' 'updated status of client client_A on round 4' 'updated status of client client_B on round 4' 'all_done=True' 'Server runner finished.';
+    for EXPECTED_OUTPUT in 'Total clients: 2' 'updated status of client client_A on round 4' 'updated status of client client_B on round 4' 'all_done=True' 'Server runner finished.' \
+                           'Start to the run Job: [0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}';
     do
-        if grep -q "$EXPECTED_OUTPUT" "$CONSOLE_OUTPUT"; then
+        if grep -q --regexp="$EXPECTED_OUTPUT" "$CONSOLE_OUTPUT"; then
             echo "Expected output $EXPECTED_OUTPUT found"
         else
             echo "Expected output $EXPECTED_OUTPUT missing"
