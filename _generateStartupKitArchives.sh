8286edbf4512a8ceaf4841db34dbe6f7f459e33b
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2025-09-30 14:44:28 +0200

    pass directory with VPN credentials as command-line argument

--- _generateStartupKitArchives.sh
+++ _generateStartupKitArchives.sh
@@ -5,12 +5,16 @@ set -e
 OUTPUT_FOLDER=workspace/`grep "^name: " $1 | sed 's/name: //'`
 TARGET_FOLDER=`ls -d $OUTPUT_FOLDER/prod_* | tail -n 1`
 LONG_VERSION=$2
-PATH_FOR_VPN_CREDENTIALS=$3
 
 cd $TARGET_FOLDER
 
 for startupkit in `ls .`; do
-    cp $PATH_FOR_VPN_CREDENTIALS/${startupkit}_client.ovpn ${startupkit}/startup/vpn_client.ovpn
+    VPN_CREDENTIALS_FILE=/vpn_credentials/${startupkit}_client.ovpn
+    if [[ -f $VPN_CREDENTIALS_FILE ]]; then
+        cp $VPN_CREDENTIALS_FILE ${startupkit}/startup/vpn_client.ovpn
+    else
+        echo "$VPN_CREDENTIALS_FILE does not exist, omitting VPN credentials for ${startupkit} in startup kit"
+    fi
     zip -rq ${startupkit}_$LONG_VERSION.zip $startupkit
     echo "Generated startup kit $TARGET_FOLDER/${startupkit}_$LONG_VERSION.zip"
 done
