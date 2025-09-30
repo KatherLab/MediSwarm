eee4a510623dbd5b0cfd186cec435917290d1261
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2025-09-30 14:44:16 +0200

    made path from where VPN credentials are copied more configurable

--- _generateStartupKitArchives.sh
+++ _generateStartupKitArchives.sh
@@ -5,12 +5,12 @@ set -e
 OUTPUT_FOLDER=workspace/`grep "^name: " $1 | sed 's/name: //'`
 TARGET_FOLDER=`ls -d $OUTPUT_FOLDER/prod_* | tail -n 1`
 LONG_VERSION=$2
-
-# TODO copy from different location
+PATH_FOR_VPN_CREDENTIALS=$3
 
 cd $TARGET_FOLDER
+
 for startupkit in `ls .`; do
-    cp ../../../tests/local_vpn/client_configs/${startupkit}_client.ovpn ${startupkit}/startup/vpn_client.ovpn
+    cp $PATH_FOR_VPN_CREDENTIALS/${startupkit}_client.ovpn ${startupkit}/startup/vpn_client.ovpn
     zip -rq ${startupkit}_$LONG_VERSION.zip $startupkit
     echo "Generated startup kit $TARGET_FOLDER/${startupkit}_$LONG_VERSION.zip"
 done
