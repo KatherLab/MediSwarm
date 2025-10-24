299a4b8af085d8039366d7355a77c8689138027c
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2025-10-24 09:27:20 +0200

    setup for local openvpn server (same as in dev-122-vpn-from-within-container branch, but without hard-coded certificates)

---
+++ tests/local_vpn/client_configs/make_ovpn.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# First argument: Client identifier
+
+KEY_DIR=./keys
+BASE_CONFIG=./client.conf
+
+cat ${BASE_CONFIG} \
+    <(echo -e '<ca>') \
+    ${KEY_DIR}/ca.crt \
+    <(echo -e '</ca>\n<cert>') \
+    ${KEY_DIR}/${1}.crt \
+    <(echo -e '</cert>\n<key>') \
+    ${KEY_DIR}/${1}.key \
+    <(echo -e '</key>\n<tls-crypt>') \
+    ${KEY_DIR}/ta.key \
+    <(echo -e '</tls-crypt>') \
+    > ${1}_client.ovpn
