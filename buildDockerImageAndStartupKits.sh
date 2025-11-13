82ebd3a0607416013787872e1689e492a2a2c882
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2025-11-13 13:58:17 +0100

    extracted copying cached pretrained model weights to separate script

--- buildDockerImageAndStartupKits.sh
+++ buildDockerImageAndStartupKits.sh
@@ -44,28 +44,7 @@ chmod a+rX . -R
 sed -i 's#__REPLACED_BY_CURRENT_VERSION_NUMBER_WHEN_BUILDING_DOCKER_IMAGE__#'$VERSION'#' docker_config/master_template.yml
 sed -i 's#__REPLACED_BY_CONTAINER_VERSION_IDENTIFIER_WHEN_BUILDING_DOCKER_IMAGE__#'$CONTAINER_VERSION_ID'#' docker_config/master_template.yml
 
-# prepare pre-trained model weights for being included in Docker image
-
-MODEL_WEIGHTS_FILE=$CWD'/docker_config/torch_home_cache/hub/checkpoints/dinov2_vits14_pretrain.pth'
-MODEL_LICENSE_FILE=$CWD'/docker_config/torch_home_cache/hub/facebookresearch_dinov2_main/LICENSE'
-if [[ ! -f $MODEL_WEIGHTS_FILE || ! -f $MODEL_LICENSE_FILE ]]; then
-    echo "Pre-trained model not available. Attempting download"
-    HUBDIR=$(dirname $(dirname $MODEL_LICENSE_FILE))
-    mkdir -p $(dirname $MODEL_WEIGHTS_FILE)
-    wget https://dl.fbaipublicfiles.com/dinov2/dinov2_vits14/dinov2_vits14_pretrain.pth -O $MODEL_WEIGHTS_FILE
-    wget https://github.com/facebookresearch/dinov2/archive/refs/heads/main.zip -O /tmp/dinov2.zip
-    unzip /tmp/dinov2.zip -d $HUBDIR
-    mv $HUBDIR/dinov2-main $HUBDIR/$(basename $(dirname $MODEL_LICENSE_FILE))
-    touch $HUBDIR/trusted_list
-fi
-
-if echo 2e405cee1bad14912278296d4f42e993 $MODEL_WEIGHTS_FILE | md5sum --check - && echo 153d2db1c329326a2d9f881317ea942e $MODEL_LICENSE_FILE | md5sum --check -; then
-    cp -r $CWD/docker_config/torch_home_cache $CLEAN_SOURCE_DIR/torch_home_cache
-else
-    exit 1
-fi
-chmod a+rX $CLEAN_SOURCE_DIR/torch_home_cache -R
-
+./_cacheAndCopyPretrainedModelWeights.sh $CWD $CLEAN_SOURCE_DIR
 cd $CWD
 
 # build and print follow-up steps
