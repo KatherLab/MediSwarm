c5384bcfe28c022a158f2e944e3413264965ccb1
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2026-01-29 13:50:22 +0100

    further debugging of failing CI

--- runIntegrationTests.sh
+++ runIntegrationTests.sh
@@ -401,7 +401,8 @@ run_dummy_training_in_swarm () {
 
     cd "$PROJECT_DIR"/prod_00
     cd admin@test.odelia/startup
-    ls -l "$CWD"/tests/integration_tests/
+    cat "$CWD"/tests/integration_tests/_submitDummyTraining.exp
+    expect -v
     expect -f "$CWD"/tests/integration_tests/_submitDummyTraining.exp
     docker kill odelia_swarm_admin_$CONTAINER_VERSION_SUFFIX
     sleep 120
