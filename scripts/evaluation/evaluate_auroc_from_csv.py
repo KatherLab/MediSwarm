ffe0e7af2a2c9a86bc1082f7b2a8b3c696af58c9
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2026-02-03 16:52:45 +0100

    store data as dicts

--- scripts/evaluation/evaluate_auroc_from_csv.py
+++ scripts/evaluation/evaluate_auroc_from_csv.py
@@ -31,25 +31,36 @@ class ConvergenceResults:
                     self.gt_classprob_for_epoch[epoch] = []
                 self.gt_classprob_for_epoch[epoch].append(GT_Classprob(row[1:]))
 
-    def print_class_statistics(self, epoch: int = 0) -> None:
+    def get_class_statistics(self, epoch: int = 0) -> None:
         gt_values = [gtcb.gt for gtcb in self.gt_classprob_for_epoch[epoch]]
+        class_statistics = {}
         for value in sorted(list(set(gt_values))):
-            print(f'{value}: {gt_values.count(value)}')
+            class_statistics[value] = gt_values.count(value)
+        return class_statistics
 
 
-def compute_auc_roc(data: List[GT_Classprob]) -> float:
-    y_true = [gtcb.gt for gtcb in data]
-    y_score = [gtcb.classprob for gtcb in data]
-    roc_auc_ovr_micro = roc_auc_score(y_true, y_score, average='micro', multi_class='ovr')
-    roc_auc_ovr_macro = roc_auc_score(y_true, y_score, average='macro', multi_class='ovr')
-    # ovo micro not implemented
-    roc_auc_ovo_macro = roc_auc_score(y_true, y_score, average='macro', multi_class='ovo')
-    print(f"ovr_micro: {roc_auc_ovr_micro:0.4f}, ovr_macro: {roc_auc_ovr_macro:0.4f}, ovo_macro: {roc_auc_ovo_macro:0.4f}")
+def compute_auc_roc(convergence_results: ConvergenceResults) -> Dict[int, float]:
+    def _compute_auc_roc(data: List[GT_Classprob]) -> float:
+        y_true = [gtcb.gt for gtcb in data]
+        y_score = [gtcb.classprob for gtcb in data]
+        roc_auc_ovr_micro = roc_auc_score(y_true, y_score, average='micro', multi_class='ovr')
+        roc_auc_ovr_macro = roc_auc_score(y_true, y_score, average='macro', multi_class='ovr')
+        # ovo micro not implemented
+        roc_auc_ovo_macro = roc_auc_score(y_true, y_score, average='macro', multi_class='ovo')
 
-    return roc_auc_ovo_macro
+        return roc_auc_ovr_micro, roc_auc_ovr_macro, roc_auc_ovo_macro
+
+    roc_auc_values:Dict[int, float] = {}
+    for epoch, data in convergence_results.gt_classprob_for_epoch.items():
+        roc_auc_ovr_micro, roc_auc_ovr_macro, roc_auc_ovo_macro = _compute_auc_roc(data)
+        print(f"epoch {epoch:02d}: ovr_micro: {roc_auc_ovr_micro:0.4f}, ovr_macro: {roc_auc_ovr_macro:0.4f}, ovo_macro: {roc_auc_ovo_macro:0.4f}")
+        roc_auc_values[epoch] = roc_auc_ovo_macro
+
+    return roc_auc_values
 
 if __name__ == '__main__':
-    cr_test = ConvergenceResults(sys.argv[1])
-    cr_test.print_class_statistics()
-    for epoch, data in cr_test.gt_classprob_for_epoch.items():
-        compute_auc_roc(data)
+    convergence_results = ConvergenceResults(sys.argv[1])
+    class_statistics = convergence_results.get_class_statistics()
+    print(class_statistics)
+    roc_auc_values = compute_auc_roc(convergence_results)
+    print(roc_auc_values)
