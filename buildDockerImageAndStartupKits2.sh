f3c567ed84989c4ce7bdd77757da98fdd1266952
Debora J <76160188+deboraJ1@users.noreply.github.com>
2026-03-13 14:43:45 +0100

    Refactor Docker build script to include also uncommitted files for challenge folder

--- buildDockerImageAndStartupKits2.sh
+++ buildDockerImageAndStartupKits2.sh
@@ -3,10 +3,10 @@
 set -e
 
 # make sure we are building from a state without local changes
-#if ! git diff --quiet || ! git diff --staged --quiet ; then
-#   echo "Local changes exist, aborting"
-#   exit 1
-#fi
+if ! git diff --quiet || ! git diff --staged --quiet ; then
+   echo "Local changes exist, aborting"
+   exit 1
+fi
 
 DOCKER_BUILD_ARGS="--no-cache --progress=plain";
 
@@ -31,8 +31,11 @@ CONTAINER_VERSION_ID=`git rev-parse --short HEAD`
 
 CWD=`pwd`
 CLEAN_SOURCE_DIR=`mktemp -d`
-mkdir -p $CLEAN_SOURCE_DIR/MediSwarm
+mkdir $CLEAN_SOURCE_DIR/MediSwarm
 git archive --format=tar HEAD | tar x -C $CLEAN_SOURCE_DIR/MediSwarm/
+# add model files manually:
+cp ./application/jobs/ODELIA_ternary_classification/app/custom/models/challenge/* $CLEAN_SOURCE_DIR/MediSwarm/application/jobs/ODELIA_ternary_classification/app/custom/models/challenge/*
+
 cd docker_config/NVFlare
 git archive --format=tar HEAD | tar x -C $CLEAN_SOURCE_DIR/MediSwarm/docker_config/NVFlare
 cd ../..
@@ -44,19 +47,13 @@ chmod a+rX . -R
 sed -i 's#__REPLACED_BY_CURRENT_VERSION_NUMBER_WHEN_BUILDING_DOCKER_IMAGE__#'$VERSION'#' docker_config/master_template.yml
 sed -i 's#__REPLACED_BY_CONTAINER_VERSION_IDENTIFIER_WHEN_BUILDING_DOCKER_IMAGE__#'$CONTAINER_VERSION_ID'#' docker_config/master_template.yml
 
-echo "Call chache and copy pretrained model weights script" 
-bash ./_cacheAndCopyPretrainedModelWeights.sh $CWD $CLEAN_SOURCE_DIR
+./_cacheAndCopyPretrainedModelWeights.sh $CWD $CLEAN_SOURCE_DIR
 cd $CWD
 
-echo "current working dir: `$CWD`"
-
 # build and print follow-up steps
-CONTAINER_NAME=`grep "      docker_image: " $PROJECT_FILE | sed 's/      docker_image: //' | sed 's#__REPLACED_BY_CURRENT_VERSION_NUMBER_WHEN_BUILDING_STARTUP_KITS__#'$VERSION'#' | tr -d '\r'`
-echo "Container Name: $CONTAINER_NAME"
-echo "Docker build args: $DOCKER_BUILD_ARGS"
-echo "Clean source dir: $CLEAN_SOURCE_DIR"
+CONTAINER_NAME=`grep "      docker_image: " $PROJECT_FILE | sed 's/      docker_image: //' | sed 's#__REPLACED_BY_CURRENT_VERSION_NUMBER_WHEN_BUILDING_STARTUP_KITS__#'$VERSION'#'`
+echo $CONTAINER_NAME
 
-echo "--> build command: docker build $DOCKER_BUILD_ARGS -t $CONTAINER_NAME $CLEAN_SOURCE_DIR -f docker_config/Dockerfile_ODELIA"
 docker build $DOCKER_BUILD_ARGS -t $CONTAINER_NAME $CLEAN_SOURCE_DIR -f docker_config/Dockerfile_ODELIA
 
 echo "Docker image $CONTAINER_NAME built successfully"
