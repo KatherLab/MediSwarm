d7e0125112d3153aa20d88df560910798e5f7427
Debora Jutz <debora.jutz@rwth-aachen.de>
2026-03-19 14:55:33 +0100

    include pretraining weights for team 1

--- application/jobs/ODELIA_ternary_classification/app/custom/threedcnn_ptl.py
+++ application/jobs/ODELIA_ternary_classification/app/custom/threedcnn_ptl.py
@@ -240,8 +240,9 @@ def prepare_training(logger, max_epochs: int, model_variant: str):
                 spec = importlib.util.spec_from_file_location("divide_model", model_creator_path)
                 divide_model_module = importlib.util.module_from_spec(spec)
                 spec.loader.exec_module(divide_model_module)
-                model = divide_model_module.create_model(n_input_channels=3, num_classes=num_classes)
-                pass
+                model = divide_model_module.ResidualEncoderClsLightning(in_ch=1, out_ch=2)
+                model.load_pretrained_unet_encoder("./models/challenge/1DivideAndConquer/checkpoint_final.pth", verbose=True)
+
             elif team_name == "2BCN_AIM":
                 model_creator_path = os.path.join(
                     os.path.dirname(Path(__file__)),
