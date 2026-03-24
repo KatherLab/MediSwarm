58f09d8072239c30b4340bd3c3c6b56e21366c22
Debora Jutz <debora.jutz@rwth-aachen.de>
2026-03-24 17:56:32 +0100

    correct imports

--- application/jobs/challenge_2BCN_AIM/app/custom/models/__init__.py
+++ application/jobs/challenge_2BCN_AIM/app/custom/models/__init__.py
@@ -3,8 +3,6 @@ This package initializes the necessary modules and classes for the project.
 """
 
 from .base_model import VeryBasicModel, BasicModel, BasicClassifier
-from .resnet import ResNet
-from .mst import MST
-from .swin3D import Swin3D
+from .swinunetr import SwinUNETR, SwinUNETRMultiTask
 
-__all__ = ['VeryBasicModel', 'BasicModel', 'BasicClassifier', 'ResNet', 'MST', "Swin3D"]
+__all__ = ['VeryBasicModel', 'BasicModel', 'BasicClassifier', 'SwinUNETR', 'SwinUNETRMultiTask']
