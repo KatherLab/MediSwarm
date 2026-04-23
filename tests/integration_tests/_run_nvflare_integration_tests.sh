9bf32e0c4412fdf0b67fc015c2876e27336d32c2
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2026-04-23 07:08:13 +0000

    fixed relative path

--- tests/integration_tests/_run_nvflare_integration_tests.sh
+++ tests/integration_tests/_run_nvflare_integration_tests.sh
@@ -7,7 +7,7 @@ run_nvflare_integration_tests () {
     cd /MediSwarm/docker_config/NVFlare
     cd tests/integration_test
     for backend in numpy tensorflow pytorch overseer ha auth preflight cifar auto stats xgboost client_api client_api_qa; do
-        run_integration_tests.sh -m $backend
+        ./run_integration_tests.sh -m $backend
     done
     cd ..
 }
