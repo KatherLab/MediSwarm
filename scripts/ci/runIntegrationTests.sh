1d511a7c505e23421f9470cd5447ea3e16606f41
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2026-04-29 09:19:51 +0000

    added option run start NVFlare unit and integration tests separately

--- scripts/ci/runIntegrationTests.sh
+++ scripts/ci/runIntegrationTests.sh
@@ -835,12 +835,16 @@ case "$1" in
 
     run_nvflare_unit_tests)
         run_nvflare_unit_tests
-        # TODO add to CI or "all" section if we want this (takes several minutes and fails for insufficient GPU memory)
         ;;
 
     run_nvflare_integration_tests)
         run_nvflare_integration_tests
-        # TODO add to CI or "all" section if we want this
+        ;;
+
+    run_nvflare_unit_and_integration_tests)
+        # TODO add to CI or "all" section if we want this (takes several minutes and fails for insufficient GPU memory)
+        run_nvflare_unit_tests
+        run_nvflare_integration_tests
         ;;
 
     run_dummy_training_standalone)
