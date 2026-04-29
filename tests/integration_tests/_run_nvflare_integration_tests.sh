bd1551632ffa2d4fe9409f16c5eb655bc79c3e07
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2026-04-29 11:22:47 +0000

    consistently set permissions as part of prepare()

--- tests/integration_tests/_run_nvflare_integration_tests.sh
+++ tests/integration_tests/_run_nvflare_integration_tests.sh
@@ -4,10 +4,10 @@ prepare () {
     pip install -U protobuf
     pip install pytest tensorflow torch
     export PATH=~/.local/bin:$PATH
+    chmod a+rwX /MediSwarm -R
 }
 
 run_nvflare_integration_tests () {
-    chmod a+rwX /MediSwarm -R
     cd /MediSwarm/docker_config/NVFlare
     cd tests/integration_test
     for backend in numpy tensorflow pytorch overseer ha auth preflight cifar auto stats xgboost client_api client_api_qa; do
