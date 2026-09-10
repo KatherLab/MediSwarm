7a46c04a786ec44c656b04807e15ad551ac36979
Ole Schwen <ole.schwen@mevis.fraunhofer.de>
2026-09-10 09:41:14 +0200

    fixed folder names

--- scripts/ci/runIntegrationTests.sh
+++ scripts/ci/runIntegrationTests.sh
@@ -680,10 +680,10 @@ verify_wrong_certificates_are_rejected () {
     echo "[Run] Verify that client and admin console with invalid certificate in startup kit do not connect ..."
 
     # start server
-    WRONG_CLIENT_DIR="$PROJECT_DIR"/../odelia_1.0.1-dev.250919.095c1b7_dummy_project_for_testing/prod_wrong_client
-    mkdir -p "$WRONG_CLIENT_DIR"
-    cp -r "$PROJECT_DIR"/prod_01 "$WRONG_CLIENT_DIR"
-    cd "$WRONG_CLIENT_DIR"
+    WRONG_STARTUP_DIR="$PROJECT_DIR"/../odelia_1.0.1-dev.250919.095c1b7_dummy_project_for_testing
+    mkdir -p "$WRONG_STARTUP_DIR"
+    cp -r "$PROJECT_DIR"/prod_01 "$WRONG_STARTUP_DIR"/prod_wrong_client
+    cd "$WRONG_STARTUP_DIR"/prod_wrong_client
     cd localhost/startup
     ./docker.sh --no_pull --start_server
     cd ../..
@@ -739,7 +739,7 @@ verify_wrong_certificates_are_rejected () {
     # cleanup
     docker kill odelia_swarm_server_flserver_$CONTAINER_VERSION_SUFFIX odelia_swarm_client_client_A_$CONTAINER_VERSION_SUFFIX
     sleep 3
-    rm -rf "$PROJECT_DIR"/prod_wrong_client
+    rm -rf "$WRONG_STARTUP_DIR"
 
     cd "$CWD"
 }
